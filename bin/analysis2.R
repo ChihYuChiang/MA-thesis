1f4553e79e44941ba49a163de84cb3a538fbb3da
ChihYuChiang <ckccyu@gmail.com>
2017-12-05 18:12:45 -0800

    Add: dist plotting

--- bin/analysis2.R
+++ bin/analysis2.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(data.table)
+library(colorspace)
 
 #Read in as DT
 DT <- fread("../data/survey2.csv")
@@ -13,7 +14,6 @@ DT <- fread("../data/survey2.csv")
 #--Select target columns
 #Personality: 1_24 2_135; SDT: 1_246 2_246
 targetColIndex <- matches("(^Person.+((1_[24])|(2_[135]))$)|(^SDT.+_[246]$)", vars=names(DT))
-DT[, targetColIndex, with=FALSE]
 
 
 #--Reverse 1-7 likert
@@ -43,17 +43,48 @@ DT[, (newColName) := personalities]
 
 #--SDT (3 constructs; 4 items each)
 #Computation
-subColIndex_1 <- matches("^SDT.+1_\\d$", vars=names(DT))
-subColIndex_2 <- matches("^SDT.+2_\\d$", vars=names(DT))
-temp <- (DT[, subColIndex_1, with=FALSE] + DT[, subColIndex_2, with=FALSE]) / 2
-
-subColIndex_3 <- matches("^SDT.+1_[135]$", vars=names(temp))
-subColIndex_4 <- matches("^SDT.+1_[246]$", vars=names(temp))
-SDTs <- (temp[, subColIndex_3, with=FALSE] + temp[, subColIndex_4, with=FALSE]) / 2
+subColIndex_1 <- matches("^SDT.+1_[135]$", vars=names(DT))
+subColIndex_2 <- matches("^SDT.+1_[246]$", vars=names(DT))
+subColIndex_3 <- matches("^SDT.+2_[135]$", vars=names(DT))
+subColIndex_4 <- matches("^SDT.+2_[246]$", vars=names(DT))
+SDTs <- (DT[, subColIndex_1, with=FALSE] + DT[, subColIndex_2, with=FALSE] + DT[, subColIndex_3, with=FALSE] + DT[, subColIndex_4, with=FALSE]) / 4
 
 #Substitution
 newColName <- gsub("1_", "", grep("^SDT.+1_[123]$", names(DT), value=TRUE))
 DT[, (newColName) := SDTs]
 
-DT[, ]
-t.test(df_player$game_agreeableness, df_player$real_agreeableness, paired=TRUE)
+
+
+
+"
+## Distribution
+"
+#--Personality
+#Function for distribution
+dist_personality <- function(DT, personality, types){
+  #A map for personality code and str pairs
+  personaCodec <- c("1"="Extraversion", "2"="Agreeableness", "3"="Conscientiousness;", "4"="Emotion stability", "5"="Openness")
+  typeCodec <- c("InS"="In-game / Self", "OutS"="Real / Self", "IdS"="Ideal / Self", "InF"="In-game / Fellow", "OutF"="Real / Fellow", "SteS"="Stereotype / Public")
+  
+  #Acquire specific columns of that personality
+  targetColIndex <- matches(sprintf("^Person.+-%s$", personality), vars=names(DT))
+  
+  make_hist <- function(type) {
+    geom_histogram(mapping=aes_(x=as.name(sprintf("Person%s-%s", type, personality)), fill=toString(which(types == type))),
+                   binwidth=0.5, alpha=0.6)
+  }
+  geom_hists <- lapply(types, make_hist)
+  
+  #Use a list to add ggplot components
+  ggplot(data=DT[, targetColIndex, with=FALSE]) +
+    geom_hists +
+    scale_x_continuous(breaks=seq(1, 7), minor_breaks=NULL, labels=seq(1, 7), limits=c(0.5, 7.5)) +
+    labs(x="score", title=personaCodec[toString(personality)]) +
+    scale_fill_manual(values=diverge_hcl(length(types)), name="Item", labels=unname(typeCodec[unlist(types)])) +
+    theme_minimal()
+}
+
+
+#Function call
+#InS OutS IdS InF OutF SteS
+dist_personality(DT, 4, list("IdS"))
