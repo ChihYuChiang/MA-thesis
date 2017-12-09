a864ca85ed765689d597c4903f6836340aa5c9c9
ChihYuChiang <ckccyu@gmail.com>
2017-12-09 00:06:03 -0600

    Add: bootstrap and fluidrow

--- report/shiny/ui.R
+++ report/shiny/ui.R
@@ -5,6 +5,8 @@ Initialization
 "
 library(shiny)
 
+PLOT_WIDTH <- "600px" 
+
 
 
 
@@ -17,7 +19,12 @@ library(shiny)
 Front end
 ------------------------------------------------------------
 "
-ui <- fluidPage(#--Set up title
+ui <- fluidPage(#--Header
+                #CSS
+                tags$head(tags$link(rel="stylesheet", type="text/css", href="main.css")),
+                tags$head(tags$link(rel="stylesheet", type="text/css", href="bootstrap-4.0.0-beta.2-dist/css/bootstrap.min.css")),
+  
+                #--Set up title
                 #Tab title
                 title='Fellow version supplement',
                 
@@ -29,29 +36,37 @@ ui <- fluidPage(#--Set up title
                 
                 tabsetPanel(
                   tabPanel("Personality",
-                             checkboxGroupInput("type_personality",
-                                                "",
-                                                c("In-game / Self"="InS",
-                                                  "Real / Self"="OutS",
-                                                  "Ideal / Self"="IdS",
-                                                  "In-game / Fellow"="InF",
-                                                  "Real / Fellow"="OutF",
-                                                  "Stereotype / Public"="SteS")),
-                             actionButton("distButton_personality", "Draw distribution"),
-                             plotOutput("dist_personality_1"),
-                             plotOutput("dist_personality_2"),
-                             plotOutput("dist_personality_3"),
-                             plotOutput("dist_personality_4"),
-                             plotOutput("dist_personality_5")),
+                           fluidRow(column(width=2,
+                                           checkboxGroupInput("type_personality",
+                                                              "",
+                                                              c("In-game / Self"="InS",
+                                                                "Real / Self"="OutS",
+                                                                "Ideal / Self"="IdS",
+                                                                "In-game / Fellow"="InF",
+                                                                "Real / Fellow"="OutF",
+                                                                "Stereotype / Public"="SteS")),
+                                           actionButton("distButton_personality", "Draw distribution")),
+                                    column(width=8,
+                                           plotOutput("dist_personality_1", width=PLOT_WIDTH),
+                                           plotOutput("dist_personality_2", width=PLOT_WIDTH),
+                                           plotOutput("dist_personality_3", width=PLOT_WIDTH),
+                                           plotOutput("dist_personality_4", width=PLOT_WIDTH),
+                                           plotOutput("dist_personality_5", width=PLOT_WIDTH)
+                                    )
+                            )),
                   tabPanel("SDT",
-                             checkboxGroupInput("type_SDT",
-                                                "",
-                                                c("In-game "="In",
-                                                  "Real"="Out",
-                                                  "Ideal"="Id")),
-                             actionButton("distButton_SDT", "Draw distribution"),
-                             plotOutput("dist_SDT_1"),
-                             plotOutput("dist_SDT_2"),
-                             plotOutput("dist_SDT_3"))
+                           fluidRow(column(width=2,
+                                           checkboxGroupInput("type_SDT",
+                                                              "",
+                                                              c("In-game "="In",
+                                                                "Real"="Out",
+                                                                "Ideal"="Id")),
+                                           actionButton("distButton_SDT", "Draw distribution")),
+                                    column(width=8,
+                                           plotOutput("dist_SDT_1", width=PLOT_WIDTH),
+                                           plotOutput("dist_SDT_2", width=PLOT_WIDTH),
+                                           plotOutput("dist_SDT_3", width=PLOT_WIDTH)
+                                    )
+                            ))
                 )
 )
