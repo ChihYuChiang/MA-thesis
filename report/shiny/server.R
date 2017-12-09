531550abbb176a9afaba2edfbd656038a8cd2f1e
ChihYuChiang <ckccyu@gmail.com>
2017-12-08 22:26:02 -0600

    Update: inter report person dist

--- report/shiny/server.R
+++ report/shiny/server.R
@@ -3,27 +3,116 @@
 Initialization
 ------------------------------------------------------------
 "
-#Basics
 library(shiny)
 library(DT)
 library(tidyverse)
 library(scales)
 library(data.table)
-library(Matrix)
-library(SparseM)
-library(feather)
+library(colorspace)
+
+DT <- fread("../../data/survey2.csv")
 
 
 
 
 "
-Prepare raw data
+## Reverse (1-7 Likert) target responses
 "
-#--Read in predicted scores (probability) of the 7 genres
-#../data/output/df_predicted.csv
-df_predicted <- fread("data/df_predicted.csv", header=TRUE) %>%
-  select(-Review) %>%
-  filter(Source == 1) #Preserve only from GameRadar
+#--Select target columns
+#Personality: 1_24 2_135; SDT: 1_246 2_246
+targetColIndex <- matches("(^Person.+((1_[24])|(2_[135]))$)|(^SDT.+_[246]$)", vars=names(DT))
+
+
+#--Reverse 1-7 likert
+reversed <- 8 - DT[, targetColIndex, with=FALSE]
+
+
+#--Assign back to DT
+#Use parenthesis since the synax does not allow with=FALSE here
+DT[, (targetColIndex) := reversed]
+
+
+
+
+"
+## Combine sub-items
+"
+#--personalities (5 constructs; 2 items each)
+#Computation
+subColIndex_1 <- matches("^Person.+1_\\d$", vars=names(DT))
+subColIndex_2 <- matches("^Person.+2_\\d$", vars=names(DT))
+personalities <- (DT[, subColIndex_1, with=FALSE] + DT[, subColIndex_2, with=FALSE]) / 2
+
+#Substitution
+newColName <- gsub("1_", "", grep("^Person.+1_\\d$", names(DT), value=TRUE))
+DT[, (newColName) := personalities]
+
+
+#--SDT (3 constructs; 4 items each)
+#Computation
+subColIndex_1 <- matches("^SDT.+1_[135]$", vars=names(DT))
+subColIndex_2 <- matches("^SDT.+1_[246]$", vars=names(DT))
+subColIndex_3 <- matches("^SDT.+2_[135]$", vars=names(DT))
+subColIndex_4 <- matches("^SDT.+2_[246]$", vars=names(DT))
+SDTs <- (DT[, subColIndex_1, with=FALSE] + DT[, subColIndex_2, with=FALSE] + DT[, subColIndex_3, with=FALSE] + DT[, subColIndex_4, with=FALSE]) / 4
+
+#Substitution
+newColName <- gsub("1_", "", grep("^SDT.+1_[123]$", names(DT), value=TRUE))
+DT[, (newColName) := SDTs]
+
+
+
+
+"
+Function for distribution
+"
+#Personality
+dist_personality <- function(DT, personality, types){
+  #A map for personality code and str pairs
+  personaCodec <- c("1"="Extraversion", "2"="Agreeableness", "3"="Conscientiousness;", "4"="Emotion stability", "5"="Openness")
+  typeCodec <- c("InS"="In-game / Self", "OutS"="Real / Self", "IdS"="Ideal / Self", "InF"="In-game / Fellow", "OutF"="Real / Fellow", "SteS"="Stereotype / Public")
+  
+  #Acquire specific columns of that personality
+  targetColIndex <- matches(sprintf("^Person.+-%s$", personality), vars=names(DT))
+  
+  make_hist <- function(type) {
+    geom_histogram(mapping=aes_(x=as.name(sprintf("Person%s-%s", type, personality)), fill=toString(which(types == type))),
+                   binwidth=0.5, alpha=0.6)
+  }
+  geom_hists <- lapply(types, make_hist)
+  
+  #Use a list to add ggplot components
+  ggplot(data=DT[, targetColIndex, with=FALSE]) +
+    geom_hists +
+    scale_x_continuous(breaks=seq(1, 7), minor_breaks=NULL, labels=seq(1, 7), limits=c(0.5, 7.5)) +
+    labs(x="score", title=personaCodec[toString(personality)]) +
+    scale_fill_manual(values=diverge_hcl(length(types)), name="Item", labels=unname(typeCodec[unlist(types)])) +
+    theme_minimal()
+}
+
+#SDT
+dist_SDT <- function(DT, SDT, types){
+  #A map for personality code and str pairs
+  SDTCodec <- c("1"="Autonomy", "2"="Relatedness", "3"="Competence")
+  typeCodec <- c("In"="In-game", "Out"="Real", "Id"="Ideal")
+  
+  #Acquire specific columns of that personality
+  targetColIndex <- matches(sprintf("^SDT.+-%s$", SDT), vars=names(DT))
+  
+  make_hist <- function(type) {
+    geom_histogram(mapping=aes_(x=as.name(sprintf("SDT%s-%s", type, SDT)), fill=toString(which(types == type))),
+                   binwidth=0.5, alpha=0.6)
+  }
+  geom_hists <- lapply(types, make_hist)
+  
+  #Use a list to add ggplot components
+  ggplot(data=DT[, targetColIndex, with=FALSE]) +
+    geom_hists +
+    scale_x_continuous(breaks=seq(1, 7), minor_breaks=NULL, labels=seq(1, 7), limits=c(0.5, 7.5)) +
+    labs(x="score", title=SDTCodec[toString(SDT)]) +
+    scale_fill_manual(values=diverge_hcl(length(types)), name="Item", labels=unname(typeCodec[unlist(types)])) +
+    theme_minimal()
+}
 
 
 
@@ -41,49 +130,43 @@ server <- function(input, output) {
   "
   Process outputs
   "
-  #--Acquire search result titles
-  searchResultTb.out <- eventReactive(input$searchButton, {
-    #Validate the input text to not be empty
-    validate(
-      need(input$searchText != "", "Please enter valid text to search")
-    )
-    
-    #Acquire the result game titles
-    searchResult <- fuzzyMatch(input$searchText, df_main["Game Title"])
-    
-    #Filter df_main with the result game titles
-    searchResultTb <- filter(df_main, `Game Title` %in% searchResult[[1]])
-    
-    #Sort the df_main according to search result distance order
-    searchResultTb[match(searchResult[[1]], searchResultTb[["Game Title"]]), ] 
+  #--Acquire dist
+  dist_personality_1.out <- eventReactive(input$distButton_personality, {
+    dist_personality(DT, 1, input$type_personality)
+  })
+  dist_personality_2.out <- eventReactive(input$distButton_personality, {
+    dist_personality(DT, 2, input$type_personality)
+  })
+  dist_personality_3.out <- eventReactive(input$distButton_personality, {
+    dist_personality(DT, 3, input$type_personality)
+  })
+  dist_personality_4.out <- eventReactive(input$distButton_personality, {
+    dist_personality(DT, 4, input$type_personality)
+  })
+  dist_personality_5.out <- eventReactive(input$distButton_personality, {
+    dist_personality(DT, 5, input$type_personality)
   })
   
   
-
   
-
-
-
-    
+  
   "
   Render output
   "
-  #--Objects
-  #Search result
-  output$searchResult <- DT::renderDataTable({
-    DT::datatable(searchResultTb.out() %>%
-                    mutate(` ` = `Game Title`) %>%
-                    select(`Game Title`, `Release Date`, ` `), #Display only title, release date, and review link
-                  selection="single",
-                  options=list(pageLength=10, dom="tip", columnDefs=list(
-                    list(targets=3, #Transform the 3rd column into a link
-                         render=JS(
-                           "function(data, type, row, meta) {",
-                           "return '<span title=\"' + data + '\"><a href=\"https://www.google.com/search?q=' + data + ' video game\">Click to google this game</a></span>';",
-                           "}")),
-                    list(targets="_all", #Center text in all column
-                         className="dt-center")
-                  )))
+  #--Render dist
+  output$dist_personality_1 <- renderPlot({
+    dist_personality_1.out()
   })
-
+  output$dist_personality_2 <- renderPlot({
+    dist_personality_2.out()
+  })
+  output$dist_personality_3 <- renderPlot({
+    dist_personality_3.out()
+  })
+  output$dist_personality_4 <- renderPlot({
+    dist_personality_4.out()
+  })
+  output$dist_personality_5 <- renderPlot({
+    dist_personality_5.out()
+  })
 }
--- report/shiny/ui.R
+++ report/shiny/ui.R
@@ -17,82 +17,33 @@ library(shiny)
 Front end
 ------------------------------------------------------------
 "
-ui <- fluidPage(#--Header
-                #CSS
-                tags$head(tags$link(rel="stylesheet", type="text/css", href="main.css")),
-  
-  
-                #--Set up title
+ui <- fluidPage(#--Set up title
                 #Tab title
-                title='Video Game Experiential Features',
+                title='Fellow version supplement',
                 
                 #Displayed title
-                titlePanel(HTML('<h2>Video Game Experiential Features
+                titlePanel(HTML('<h2>Fellow version report supplement
                                   <span id="authorship">Chih-Yu Chiang • chihyuchiang@uchicago.edu</span>
                                  </h2>')),
                 
                 
                 #--Decide primary layout
                 sidebarLayout(
-                  #Side bar                
                   sidebarPanel(
-                    h3("Instruction"),
-                    HTML("<p>
-                           This app displays the <b>experiential features</b> of 11,724 video games of the last several decades. Based on these features, one can explore the experiences delivered by those video games. Regarding their in-game experiences, this app helps identify similar games and portrays the experiences by keywords that distinguishing a specific title from the other games.
-                          </p>"),
-                    tags$ol(tags$li("Please use video game names to search a game title you intend to explore."),
-                            tags$li("Select a target game title by clicking on the search result.", img(src="instruct_1.png", height="150"))),
-                    HTML("<p>
-                           The games' experiential features are identified through a predictive model developed in a research project of Chih-Yu Chiang, advised by James Evans and Oleg Urminsky of the University of Chicago. This model is based on multiple player surveys and expert game reviews of major video game information platforms, including GameSpot, GamesRadar, and Polygon, with a total of 15-million words of analysis. This model incorporates multiple machine learning techniques to extract experiential compositions of a video game from a video game specific Word2Vec embedding space.
-                         </p>"),
-                    HTML("<p>
-                           This app utilizes two constructs to measure the video game experiential features. One is a game's relationship to the Experiential Keyword Groups, English word sets that present corresponding human experiences. Another is a game's relationship to the Experiential Genres. They can be deemed as clusters of the Keyword Groups. For more information about these constructs, please refer to the <a href=\"https://github.com/ChihYuChiang/MAPSS-Thesis/blob/master/writing/Thesis%20-%20final%20draft.pdf\">original paper</a>.
-                         </p>"),
-                    p(class="ss-text", "(Please allow 10 seconds for initialization and each operation, the free shiny.io server is agonizingly slow)"),
-                    textInput(inputId="searchText", label="", value=""),
-                    actionButton(inputId="searchButton", label="search"),
-                    p(""), #To make a space above. Somehow the `br()` doesn't work here.
-                    DT::dataTableOutput(outputId="searchResult"),
-                    br()
-                  ),
-                  
-                  #Main panel
+                         checkboxGroupInput("type_personality",
+                                            "",
+                                            c("In-game / Self"="InS",
+                                              "Real / Self"="OutS",
+                                              "Ideal / Self"="IdS",
+                                              "In-game / Fellow"="InF",
+                                              "Real / Fellow"="OutF",
+                                              "Stereotype / Public"="SteS")),
+                         actionButton("distButton_personality", "Draw distribution")),
                   mainPanel(
-                    h3("Target Title"),
-                    DT::dataTableOutput(outputId="targetTitle"),
-                    br(),
-                    HTML("<p class=\"footnote\">
-                           ESRB Rating: The <a href=\"http://www.esrb.org/\">Entertainment Software Rating Board</a> (ESRB) assigns age and content ratings for video games and apps indicating the appropriate age group for the content.
-                           <br>
-                           GS Score: General quality score (1-10) of a video game, rated by expert video gamers who invited by <a href=\"https://www.gamespot.com/\">GameSpot</a>, a major video game information website in the U.S.
-                           <br>
-                           User Score: General quality score (1-10) of a video game, rated by general visitors of GameSpot.
-                           <br>
-                           Genre 1-7: Propensity of a video game to be assigned to a specific Genre by the algorithm.
-                          </p>"),
-                    br(), hr(),
-                    h3("Similar Titles"),                    
-                    DT::dataTableOutput(outputId="similarTitle"),
-                    br(), hr(),                   
-                    h3("Dominant Features"), 
-                    DT::dataTableOutput(outputId="dominantFeature"),
-                    br(),
-                    HTML("<p class=\"footnote\">
-                           Keyword Group: Internal ID of a Keyword Group. 
-                           <br>
-                           Similarity: -1 to 1; the higher the similiarity, the more similar the game and the target game.
-                          </p>"),
-                    br(), hr(),
-                    h3("Distingusishing Features"),
-                    h4("Strong"),
-                    DT::dataTableOutput(outputId="dStrongFeature"),
-                    br(),
-                    HTML("<p class=\"footnote\">
-                           Percent Rank: Percentage position of a video game, among all games in the database, regarding the similarity to a specific Keyword Group.
-                          </p>"),
-                    br(),
-                    h4("Weak"),
-                    DT::dataTableOutput(outputId="dWeakFeature")
-                  )
+                         plotOutput("dist_personality_1"),
+                         plotOutput("dist_personality_2"),
+                         plotOutput("dist_personality_3"),
+                         plotOutput("dist_personality_4"),
+                         plotOutput("dist_personality_5"))
                 )
 )
