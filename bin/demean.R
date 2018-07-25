8fadab10fddf621fe897f3580aff3880772a443b
ChihYuChiang <ckccyu@gmail.com>
2018-07-26 03:03:45 +0800

    Update: format of demean

--- bin/demean.R
+++ bin/demean.R
@@ -7,6 +7,6 @@ model_demean <- lm(`preference` ~ `respondent` + factor(`core_id`), DT_2_long)
 summary(model_demean)
 
 #Acquire the residules and add back the overall mean
-res_demean <- DT_2_long[, .(`respondent`, `core_id`)][, res := residuals(model_demean) + model_demean$coefficients[1]]
+res_demean <- DT_2_long[, .(`respondent`, `core_id`)][, c("res", "allmean") := .(residuals(model_demean), model_demean$coefficients[1])]
   
 write.csv(res_demean, file="../data/raw_survey/processed/res_demean.csv")
